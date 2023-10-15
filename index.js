8efc9363039a8b3ec51e9d9187ed78ce63456780
Jazper <jazpersaldana@yahoo.com>
2023-10-15 16:32:28 -0500

    first class functions lab compelte

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0, 2)
+}
+returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(2)
+}
+returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(integer) {
+    return function(fare) {
+    return integer * fare
+    }
+}
+createFareMultiplier(5)
+
+const fareDoubler = createFareMultiplier(2)
+fareDoubler(10)
+
+const fareTripler = createFareMultiplier(3)
+fareTripler(12)
+
+function selectDifferentDrivers(drivers, functions) {
+    return functions(drivers)
+}
+selectDifferentDrivers(drivers, returnFirstTwoDrivers)
+selectDifferentDrivers(drivers, returnLastTwoDrivers)
